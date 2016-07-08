d41916370190a864a2874c8be5ec6ff22c3a5011
Dmitry Chertenko <xch3dx@gmail.com>
2016-07-08 18:12:23 +0200

    add StubActivity

---
+++ app/src/main/java/com/ch3d/android/mock/StubActivityFragment.java
@@ -0,0 +1,22 @@
+package com.ch3d.android.mock;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class StubActivityFragment extends Fragment {
+
+    public StubActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_stub, container, false);
+    }
+}
